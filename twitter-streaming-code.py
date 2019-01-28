d9cadf2586f3f1beb2ffeb6b5498b344b25f19a0
rituraj17 <krishna_rituraj17@hotmail.com>
2019-01-28 15:36:24 +0530

    Update twitter-streaming-code.py

--- twitter-streaming-code.py
+++ twitter-streaming-code.py
@@ -2,13 +2,14 @@ from tweepy import Stream
 from tweepy import StreamListener
 from tweepy import OAuthHandler
 
+# enter Twitter API Keys
 consumer_key=' Enter your key'
 consumer_secret=' Enter your key'
 access_token=' Enter your key'
 access_token_secret=' Enter your key'
 
 
-
+# class which prints recieved data 
 class Listener(StreamListener):
     def on_data(self,data):
         print data
@@ -17,9 +18,11 @@ class Listener(StreamListener):
         print status
 
 if __name__ == '__main__':
+    # authentication with twitter api for streaming
     l=Listener()
     auth=OAuthHandler(consumer_key,consumer_secret)
-    auth.set_access_token(access_token,access_token_secret)
-    
+    auth.set_access_token(access_token,access_token_secret) 
     stream=Stream(auth,l)
+    
+    # filter tweet based on keywords
     stream.filter(track=['bigdata','machineleaning','devops','analytics','softwaredeveloper'])
